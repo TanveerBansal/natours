e2921bffa4703a92a8a3635c74815560aa691d2b
TanveerBansal <singhtanveer020@gmail.com>
2025-03-20 22:29:52 +0530

    send token in cookies

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,6 +12,25 @@ const signToken = (id) => {
     return jwt.sign({ id: id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN })
 }
 
+const createSendToken = (user, statusCode, res) => {
+    const token = signToken(user._id)
+    const cookieOptions = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+    }
+    if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
+    res.cookie('jwt', token, cookieOptions)
+
+    // to dont show the user password in response
+    user.password = undefined
+
+    res.status(statusCode).json({
+        status: "success",
+        token,
+        data: { user }
+    })
+}
+
 exports.signup = catchAsync(async (req, res, next) => {
     const newUser = await User.create({
         name: req.body.name,
@@ -21,15 +40,7 @@ exports.signup = catchAsync(async (req, res, next) => {
         passwordChangedAt: req.body.passwordChangedAt
     })
 
-    const token = signToken(newUser._id)
-
-    res.status(200).json({
-        status: "success",
-        token,
-        data: {
-            newUser
-        }
-    })
+    createSendToken(newUser, 201, res)
 })
 
 exports.login = catchAsync(async (req, res, next) => {
