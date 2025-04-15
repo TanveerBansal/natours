af024b038cdad47ad73e5e02003b0dcf378c52c6
TanveerBansal <singhtanveer020@gmail.com>
2025-04-15 22:16:09 +0530

    tour model update and model middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose")
 const slugify = require('slugify')
 const validator = require('validator')
+const UserModel = require("./userModal")
 
 const tourSchema = new mongoose.Schema({
     name: {
@@ -43,7 +44,29 @@ const tourSchema = new mongoose.Schema({
     images: { type: [String] },
     createdAt: { type: Date, default: Date.now(), select: false },
     startDates: [Date],
-    secretTour: { type: Boolean, default: false }
+    secretTour: { type: Boolean, default: false },
+    startLocation: {
+        type: {
+            type: String,
+            default: "Point",
+            enum: ["Point"]
+        },
+        coordinates: [Number],
+        address: String,
+        description: String
+    },
+    locations: [{
+        type: {
+            type: String,
+            default: "Point",
+            enum: ["Point"]
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number
+    }],
+    guides: Array
 },
     {
         toJSON: { virtuals: true },
@@ -59,6 +82,13 @@ tourSchema.virtual('durationWeeks').get(function () {
 // DOCUMENT MIDDLEWARE: runs before save() create() not for insert
 tourSchema.pre('save', function (next) {
     this.slug = slugify(this.name, { lower: true })
+    next()
+})
+
+//middle to embedded the guides data through ids
+tourSchema.pre('save', async function (next) {
+    const guidePromises = this.guides.map(async id => await UserModel.findById(id))
+    this.guides = await Promise.all(guidePromises)
     next()
 })
 // tourSchema.pre('save', function (next) {
