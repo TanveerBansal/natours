fb511535aca89ec1a4504cd29c014bc7619d2abb
TanveerBansal <singhtanveer020@gmail.com>
2025-04-23 22:08:59 +0530

    Nested routes for reviews

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,8 +1,11 @@
-const router = require("express").Router()
+const express = require("express")
 const reviewController = require("../controllers/reviewController")
 const authController = require("../controllers/authController")
 
-router.route("/").get(reviewController.getAllReviews).post(authController.protect,reviewController.createReview)
+// this merge params here is used as our route is in tourRouter so we need to get access of that route params
+const router = express.Router({ mergeParams: true })
+
+router.route("/").get(reviewController.getAllReviews).post(authController.protect, reviewController.createReview)
 
 
 module.exports = router
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,17 +2,29 @@ const express = require("express")
 const router = express.Router()
 const { checkId, checkBody, aliasTopTours, getMonthlyPlan, getTourStats, getAllTours, createTour, getTour, updateTour, deleteTour } = require("../controllers/tourController")
 const authController = require("../controllers/authController")
-
+const { createReview } = require("../controllers/reviewController")
+const reviewRouter = require("./reviewRoutes")
 
 
 //param middleware
 // router.param("id", checkId )
 
-router.route("/top-5-cheap").get(aliasTopTours,getAllTours)
+// NESTED ROUTE
+// example:-
+// POST /tour/:tourId/reviews
+// GET /tour/:tourId/reviews
+// GET /tour/:tourId/reviews/:reviewId
+router.use("/:tourId/reviews", reviewRouter)
+
+
+
+router.route("/top-5-cheap").get(aliasTopTours, getAllTours)
 router.route("/tour-stats").get(getTourStats)
 router.route("/monthly-plan/:year").get(getMonthlyPlan)
 
 router.route('/').get(authController.protect, getAllTours).post(createTour)
-router.route('/:id').get(getTour).patch(updateTour).delete(authController.protect, authController.restrictTo("admin","lead-guide") , deleteTour)
+router.route('/:id').get(getTour).patch(updateTour).delete(authController.protect, authController.restrictTo("admin", "lead-guide"), deleteTour)
+
+
 
 module.exports = router
